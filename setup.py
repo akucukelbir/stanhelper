2f965072cd89c917b9e4b6ba2d6e0fadf55fdffe
Alp Kucukelbir <akucukelbir@users.noreply.github.com>
2017-09-11 10:11:42 -0400

    Fix 1d list conversion + flake8 (#4)
    * updates to doc, flake8, imports, etc
    
    * always convert 1d lists as arrays

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup
 
-exec(open('stanhelper/version.py').read())
+__version__ = '1.0.3'
 
 
 setup(
-  name='stanhelper',
-  packages=['stanhelper'],
-  version=__version__,
-  description='Functions that help interface with cmdStan.',
-  author='Alp Kucukelbir',
-  author_email='alp@cs.columbia.edu',
-  url='https://github.com/akucukelbir/stanhelper',
-  download_url=('https://github.com/akucukelbir/stanhelper/releases/' +
-                __version__),
-  install_requires=['numpy', 'pandas'],
-  python_requires='>=3.5',
-  license='GPLv3',
-  keywords=[],
-  classifiers=[],
+    name='stanhelper',
+    packages=['stanhelper'],
+    version=__version__,
+    description='Functions that help interface with cmdStan.',
+    author='Alp Kucukelbir',
+    author_email='alp@cs.columbia.edu',
+    url='https://github.com/akucukelbir/stanhelper',
+    download_url=('https://github.com/akucukelbir/stanhelper/releases/' +
+                  __version__),
+    install_requires=['numpy', 'pandas'],
+    python_requires='>=3.5',
+    license='GPLv3',
+    keywords=[],
+    classifiers=[],
 )
